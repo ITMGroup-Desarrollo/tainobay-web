eeb4da9e710baa1bd11884278099ff82e8beb381
Cristian Mendoza Suarez <cmendoza@ITMGROUP.MX>
2024-08-14 16:20:23 -0500

    Added four duplicate lines of code to display blog post content.

--- blog.php
+++ blog.php
@@ -186,6 +186,10 @@ $title = isset($_GET['title']) ? $_GET['title'] : '';
                 </div>
                 <div class="blog-post blog-texto">
                     ${data.content.split('\n').map(paragraph => `<p class="justificado">${paragraph}</p>`).join('')}
+                    ${data.content.split('\n').map(paragraph => `<p class="justificado">${paragraph}</p>`).join('')}
+                    ${data.content.split('\n').map(paragraph => `<p class="justificado">${paragraph}</p>`).join('')}
+                    ${data.content.split('\n').map(paragraph => `<p class="justificado">${paragraph}</p>`).join('')}
+                    ${data.content.split('\n').map(paragraph => `<p class="justificado">${paragraph}</p>`).join('')}
                 </div>
                 <div class="title-image">
                     <img src="${data.image_url}" class="img-fluid imgRound" alt="${data.title}">
