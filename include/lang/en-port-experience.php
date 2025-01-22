35b8878ec3191509e95fa762e89439467a160dbb
Cristian Mendoza Suarez <cmendoza@ITMGROUP.MX>
2025-01-22 12:21:52 -0500

    adding photos to mr rum quest gallery

--- include/lang/en-port-experience.php
+++ include/lang/en-port-experience.php
@@ -1022,6 +1022,9 @@ $points = [
       'assets\images\port-experience\mr-rum\gallery\mr-rum_2.webp',
       'assets\images\port-experience\mr-rum\gallery\mr-rum_3.webp',
       'assets\images\port-experience\mr-rum\gallery\mr-rum_5.webp',
+      'assets\images\port-experience\mr-rum\gallery\mr-rum_6.webp',
+      'assets\images\port-experience\mr-rum\gallery\mr-rum_7.webp',
+      'assets\images\port-experience\mr-rum\gallery\mr-rum_8.webp',
     ],
   ],
 ];
